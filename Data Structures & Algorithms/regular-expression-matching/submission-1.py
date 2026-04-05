713e06dfdd1920421a5c0e12b160dd3beb9c84f5
Austin Renz <ishiba324@gmail.com>
2026-04-04 21:11:36 -0400

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        #at each point, we check if they're the same, if not can choose to . match or * however many times
+        cache = {}
+        m = len(s)
+        n = len(p)
+        def dfs(i, j):
+            if j == n:
+                return i == m
+            if (i, j) in cache:
+                return cache[(i, j)]
+            match = i < m and (s[i] == p[j] or p[j] == '.')
+            if (j + 1) < n and p[j + 1] == '*':
+                cache[(i, j)] = (dfs(i, j + 2) or (match and dfs(i + 1, j)))
+                return cache[(i, j)]
+            if match:
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            cache[(i, j)] = False
+            return False
+        return dfs(0,0)
