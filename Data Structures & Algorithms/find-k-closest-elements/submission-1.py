8d6e49efce52decfaa9a8e29f59683b83236bbf6
Austin Renz <ishiba324@gmail.com>
2026-05-26 20:07:20 -0700

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        #keep a window of k closest elements, for each new element if it is added then remove old element?
+        #only add if it is less than l's dist, if tied keep l
+        l, r = 0, len(arr) - k
+        while l < r:
+            m = (l + r) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l: l + k]
