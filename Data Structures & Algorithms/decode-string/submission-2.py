9f33413849fdec05160e271b2af409189ac2e486
Austin Renz <ishiba324@gmail.com>
2026-06-04 20:03:16 -0700

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        #some type of stack, look for ] to go back
+        stringStack = []
+        countStack = []
+        cur = ""
+        k = 0
+        for c in s:
+            if c.isdigit():
+                k = k * 10 + int(c)
+            elif c == "[":
+                stringStack.append(cur)
+                countStack.append(k)
+                cur = ""
+                k = 0
+            elif c == "]":
+                temp = cur
+                cur = stringStack.pop()
+                count = countStack.pop()
+                cur += temp * count
+            else:
+                cur += c
+        return cur
